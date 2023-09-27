74f47e1b6ed23be3c48c4a807d1977ce2d78688e
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-09-27 17:54:33 -0600

    Populate new tables

--- data.sql
+++ data.sql
@@ -1,5 +1,7 @@
 /* Populate database with sample data. */
 
+/*ANIMALS TABLE*/
+
 INSERT INTO animals (
   name,
   date_of_birth,
@@ -153,4 +155,47 @@ VALUES (
   4,
   TRUE,
   22
+);
+
+/*OWNERS TABLE*/
+
+INSERT INTO owners(
+  full_name,
+  age
+)
+VALUES (
+  'Sam Smith',
+  34
+),
+(
+  'Jennifer Orwell',
+  19
+),
+(
+  'Bob',
+  45
+),
+(
+  'Melody Pond',
+  77
+),
+(
+  'Dean Winchester',
+  14
+),
+(
+  'Jodie Whittaker',
+  38
+);
+
+/*SPECIES TABLE*/
+
+INSERT INTO species (
+  name
+)
+VALUES (
+  'Pokemon'
+),
+(
+  'Digimon'
 );
