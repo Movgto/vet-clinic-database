2ad698a39fa3f1b471002c71d0aa16a2c8ef4219
GUMARO MONROY VAZQUEZ <gumaro.monroyv@uanl.edu.mx>
2023-09-27 10:45:51 -0600

    Update table

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
+/*Day one queries*/
+
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT * FROM animals
 WHERE date_of_birth >= '2016-01-01'
@@ -16,3 +18,61 @@ SELECT * FROM animals WHERE name NOT IN ('Gabumon');
 SELECT * FROM animals
 WHERE weight_kg >= 10.4
 AND weight_kg <= 17.3;
+
+/*Day two*/
+/*Transactions*/
+
+BEGIN;
+UPDATE animals
+SET species = 'unspecified';
+SELECT * FROM animals;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET species = 'digimon'
+WHERE name LIKE '%mon';
+UPDATE animals
+SET species = 'pokemon'
+WHERE species is NULL;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+SELECT * FROM animals;
+ROLLBACK;
+SELECT * FROM animals;
+COMMIT;
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth > '2022-01-01';
+SAVEPOINT del_after_2022;
+SELECT * FROM animals;
+UPDATE animals
+SET weight_kg = animals.weight_kg * -1;
+SELECT * FROM animals;
+ROLLBACK TO del_after_2022;
+SELECT * FROM animals;
+UPDATE animals
+SET weight_kg = animals.weight_kg * -1
+WHERE weight_kg < 0;
+SELECT * FROM animals;
+COMMIT;
+
+/*Queries*/
+
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(escape_attempts) FROM animals
+WHERE escape_attempts > 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(escape_attempts) FROM animals
+WHERE escape_attempts > 0
+GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals
+GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals
+WHERE date_of_birth > '1990-01-01'
+AND date_of_birth < '2000-01-01'
+GROUP BY species;
